fcc68276b74604776d35621c76b9d42cf2b35a95
gingerbisquit <gingerbisquit@protonmail.com>
2022-03-25 22:12:24 +0100

    dissapearing login window

--- src/com/barosanu/controller/BaseController.java
+++ src/com/barosanu/controller/BaseController.java
@@ -4,8 +4,8 @@ import com.barosanu.EmailManager;
 import com.barosanu.view.ViewFactory;
 
 public abstract class BaseController {
-    private EmailManager emailManager;
-    private ViewFactory viewFactory;
+    protected EmailManager emailManager;
+    protected ViewFactory viewFactory;
     private String fxmlName;
 
     public BaseController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -22,8 +22,19 @@ public class ViewFactory {
         System.out.println("show login window");
 
         BaseController controller = new LoginWindowController(emailManager, this,"LoginWindow.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
-        fxmlLoader.setController(controller);
+        initializeStage(controller);
+    }
+
+    public void showMainWindow(){
+        System.out.println("main window called");
+        BaseController controller = new LoginWindowController(emailManager, this,"MainWindow.fxml");
+        initializeStage(controller);
+
+    }
+
+    private void initializeStage(BaseController baseController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
         Parent parent;
         try {
             parent = fxmlLoader.load();
@@ -35,5 +46,9 @@ public class ViewFactory {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void closeStage(Stage stageToClose){
+        stageToClose.close();
     }
 }
