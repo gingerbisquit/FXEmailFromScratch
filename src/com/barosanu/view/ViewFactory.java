123287cbc398ff36085b555d6a1d54fb4ff295cc
gingerbisquit <gingerbisquit@protonmail.com>
2022-03-25 21:57:41 +0100

    ok, passed the bump with fxmlLoader...

--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -1,6 +1,15 @@
 package com.barosanu.view;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.BaseController;
+import com.barosanu.controller.LoginWindowController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailManager emailManager;
@@ -11,5 +20,20 @@ public class ViewFactory {
 
     public void showLoginWindow(){
         System.out.println("show login window");
+
+        BaseController controller = new LoginWindowController(emailManager, this,"LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
